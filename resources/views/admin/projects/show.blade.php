6cd1596c80a8bc876dd1ae7d2372612a45e2a544
Eecodev <mnasser.bbk@gmail.com>
2024-01-15 14:56:33 +0100

    route added for img to be displayed

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,5 +3,9 @@
     <section class="container">
         <h1>{{$project->title}}</h1>
         <p>{{$project->description}}</p>
+        <div>
+            <img src="{{asset('storage/' . $project->image)}}" alt="{{$project->title}}">
+        </div>
     </section>
 @endsection
+
